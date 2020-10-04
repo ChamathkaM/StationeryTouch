f9c24ec7d3433420e90e4749b65b27450d12620e
hasinii98 <hasininimaya@gmail.com>
2020-10-04 20:18:26 +0530

    Hasini R

--- app/src/main/java/com/example/stationerystore/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/stationerystore/adapter/ProductAdapter.java
@@ -18,7 +18,7 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-
+//adapter class
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
 
     private List<Product> productList;
